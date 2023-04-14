6b5b031dc17b793e50c01353fbe99b596afcad23
HPHuerta <hhernanp@gmail.com>
2023-04-13 22:03:47 -0400

    feat: font settings component added, time form separated in a dif component

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,25 +1,31 @@
 import Image from 'next/image'
+import SettingsForm from './Form/SettingsForm'
 import { useState } from 'react'
 
 interface SettingsProps {
   setShowModal: (value: boolean) => void
 }
 
+const fontArray = [
+  {
+    name: 'font-kumbh',
+    text: 'Aa'
+  },
+  {
+    name: 'font-robotoSlab',
+    text: 'Aa'
+  },
+  {
+    name: 'font-spaceMono',
+    text: 'Aa'
+  }
+]
+
 export default function Settings ({ setShowModal }: SettingsProps) {
-  const [pomodoro, setPomodoro] = useState(25)
-  const [shortBreak, setShortBreak] = useState(5)
-  const [longBreak, setLongBreak] = useState(15)
+  const [activeIndex, setActiveIndex] = useState(null)
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, setValue: React.Dispatch<React.SetStateAction<number>>) => {
-    setValue(Number(e.target.value))
-  }
-
-  const handleIncrease = (setValue: React.Dispatch<React.SetStateAction<number>>) => {
-    setValue((prevValue: number) => prevValue + 1)
-  }
-
-  const handleDecrease = (setValue: React.Dispatch<React.SetStateAction<number>>) => {
-    setValue((prevValue: number) => prevValue - 1)
+  const handleClick = (index: any) => {
+    setActiveIndex(index)
   }
 
   return (
@@ -38,93 +44,25 @@ export default function Settings ({ setShowModal }: SettingsProps) {
       </div>
       <div className='flex flex-col p-4 w-full'>
         <h2 className='font-semibold text-center uppercase text-[0.8rem] tracking-[0.2rem] pb-[1rem]'>time ( minutes )</h2>
-        {/* {TODO: Separate this from into another component} */}
-        <form className='border-b'>
-          <div className='flex justify-between w-full mb-[0.5rem]'>
-            <label className='text-[#979797] opacity-[.67] font-bold' >pomodoro</label>
-            <div className='relative flex'>
-              <input
-                type="number"
-                className='h-8 rounded-md border bg-[#EFF1FA] text-left pl-2 text-[#1E213F] font-bold w-[6rem]'
-                value={pomodoro}
-                onChange={(e) => { handleChange(e, setPomodoro) }} />
-              <Image
-                src='/images/icon-arrow-up.svg'
-                alt='arrow up icon'
-                className='fill-[#EFF1FA] absolute top-[0.5rem] place-self-center right-[0.5rem] cursor-pointer hover:fill-[#1E213F]'
-                width={10}
-                height={10}
-                onClick={() => { handleIncrease(setPomodoro) }}
-              />
-              <Image
-                src='/images/icon-arrow-down.svg'
-                alt='arrow down icon'
-                className='decoration-[#EFF1FA] absolute bottom-[0.5rem] place-self-center right-[0.5rem] cursor-pointer hover:decoration-[#1E213F]'
-                width={10}
-                height={10}
-                onClick={() => { handleDecrease(setPomodoro) }}
-              />
-            </div>
-          </div>
-          <div className='flex justify-between w-full mb-[0.5rem]'>
-            <label className='text-[#979797] opacity-[.67] font-bold' >short break</label>
-            <div className='relative flex'>
-              <input
-                type='number'
-                className='h-8 rounded-md border bg-[#EFF1FA] text-left pl-2 text-[#1E213F] font-bold w-[6rem]'
-                value={shortBreak}
-                onChange={(e) => { handleChange(e, setShortBreak) }}
-              />
-              <Image
-                src='/images/icon-arrow-up.svg'
-                alt='arrow up icon'
-                className='decoration-[#EFF1FA] absolute top-[0.5rem] place-self-center right-[0.5rem] cursor-pointer hover:decoration-[#1E213F]'
-                width={10}
-                height={10}
-                onClick={() => { handleIncrease(setShortBreak) }}
-              />
-              <Image
-                src='/images/icon-arrow-down.svg'
-                alt='arrow down icon'
-                className='decoration-[#EFF1FA] absolute bottom-[0.5rem] place-self-center right-[0.5rem] cursor-pointer hover:decoration-[#1E213F]'
-                width={10}
-                height={10}
-                onClick={() => { handleDecrease(setShortBreak) }}
-              />
-            </div>
-          </div>
-          <div className='flex justify-between w-full mb-[1rem]'>
-            <label className='text-[#979797] opacity-[.67] font-bold' >long break</label>
-            <div className='relative flex'>
-              <input
-                type='number'
-                className='h-8 rounded-md border bg-[#EFF1FA] text-left pl-2 text-[#1E213F] font-bold w-[6rem]'
-                value={longBreak}
-                onChange={(e) => { handleChange(e, setLongBreak) }}
-              />
-              <Image
-                src='/images/icon-arrow-up.svg'
-                alt='arrow up icon'
-                className='decoration-[#EFF1FA] absolute top-[0.5rem] place-self-center right-[0.5rem] cursor-pointer hover:decoration-[#1E213F]'
-                width={10}
-                height={10}
-                onClick={() => { handleIncrease(setLongBreak) }}
-              />
-              <Image
-                src='/images/icon-arrow-down.svg'
-                alt='arrow down icon'
-                className='decoration-[#EFF1FA] absolute bottom-[0.5rem] place-self-center right-[0.5rem] cursor-pointer hover:decoration-[#1E213F]'
-                width={10}
-                height={10}
-                onClick={() => { handleDecrease(setLongBreak) }}
-              />
-            </div>
-          </div>
-        </form>
+        <SettingsForm />
         {/* {TODO: this can be another component} */}
         <div className='pt-[1rem] border-b'>
           <h2 className='font-semibold text-center uppercase text-[0.8rem] tracking-[0.2rem] pb-[1rem]'>font</h2>
-          <div className='flex justify-between w-full mb-[0.5rem]'></div>
+          <div className='flex justify-center gap-[1rem] w-full mb-[0.5rem]'>
+            {fontArray.map((font, index) => {
+              return (
+                <div key={index} className='flex items-center'>
+                  <button
+                    className={`rounded-full w-[3rem] h-[3rem] hover:border ${activeIndex === index
+                      ? 'bg-[#161932] text-[#FFFFFF]'
+                      : 'flex justify-center items-center  bg-[#EFF1FA] font-bold'} `}
+                    onClick={() => { handleClick(index) }}>
+                    <p className={`font-bold text-[0.8rem] ${font.name}`}>{font.text}</p>
+                  </button>
+                </div>
+              )
+            })}
+          </div>
         </div>
 
       </div>
