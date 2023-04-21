82ffe1ef6158a65e862631d3c07b2daad5087d42
HPHuerta <hhernanp@gmail.com>
2023-04-21 01:21:11 -0400

    feat: timer configuration, timer format added

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,5 +1,8 @@
 import { type ColorContextProps } from '@/lib/context/ColorContext'
+import { type TimeContextProps } from '@/lib/context/TimeContext'
 import useColor from '@/lib/hooks/useColor'
+import useTime from '@/lib/hooks/useTime'
+import { useEffect, useState } from 'react'
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
 
@@ -9,12 +12,38 @@ interface TimerProps {
 
 export default function Timer ({ time }: TimerProps) {
   const { color } = useColor() as ColorContextProps
+  const { pomodoro, shortBreak, longBreak } = useTime() as TimeContextProps
+  const [mode, setMode] = useState<'pomodoro' | 'shortBreak' | 'longBreak'>('pomodoro')
+  // const [status, setStatus] = useState<'stopped' | 'running' | 'paused'>('stopped')
+  const [isPaused, setIsPaused] = useState(false)
+  const [timeLeft, setTimeLeft] = useState(0)
+
+  const initTimer = () => {
+    setTimeLeft(pomodoro * 60)
+  }
+
+  useEffect(() => {
+    initTimer()
+  }, [pomodoro])
+
+  let totalSeconds = mode === 'pomodoro' ? pomodoro * 60 : shortBreak * 60
+
+  if (mode === 'longBreak') totalSeconds = longBreak * 60
+
+  const percentage = Math.round((timeLeft / totalSeconds) * 100)
+
+  const minutes = Math.floor(timeLeft / 60)
+  let seconds = timeLeft - minutes * 60
+  if (seconds < 10) {
+    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+    seconds = '0' + seconds
+  }
 
   return (
     <div className='relative w-[14rem] h-[14rem] bg-gradient-to-br shadow-2xl shadow-[#D7E0FF]/20 from-[#161932] from-20% via-[#1E213F] to-[#212840] p-[0.8rem] rounded-full'>
       <CircularProgressbar
-        value={time}
-        text={`${time}`}
+        value={percentage}
+        text={`${minutes}:${seconds}`}
         background={true}
         backgroundPadding={5}
         strokeWidth={4}
@@ -25,7 +54,11 @@ export default function Timer ({ time }: TimerProps) {
           backgroundColor: '#161932'
         })}
       />
-      <h2 className='absolute bottom-[3.8rem] left-[4.8rem] text-[#D7E0FF] uppercase font-normal text-[0.8rem] tracking-[0.5rem]'>pause</h2>
+      {
+        !isPaused
+          ? <h2 className='absolute bottom-[3.8rem] left-[4.8rem] text-[#D7E0FF] uppercase font-normal text-[0.8rem] tracking-[0.5rem]'>pause</h2>
+          : <h2 className='absolute bottom-[3.8rem] left-[5.3rem] text-[#D7E0FF] uppercase font-normal text-[0.8rem] tracking-[0.5rem]'>play</h2>
+      }
     </div>
 
   )
